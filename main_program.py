f2fd271f1291d00d035674fccb44315db7d7128a
tkaralidi <32501075+tkaralidi@users.noreply.github.com>
2021-02-22 14:19:27 -0500

    Create main_program.py
    this is the skeleton of the main program

---
+++ main_program.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+
+#create array of x coordinates:
+
+x = -10. + np.arange(0, 20.1, 0.1) 
+
+# ask for input R:
+
+
+# call the external functions:
+
+
+# print informative statements:
